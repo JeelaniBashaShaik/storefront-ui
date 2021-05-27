14a88e33764aef9f5f163f6496aa6d86a1e8d340
Jeelani <jeelanshaik07@gmail.com>
2021-05-27 23:52:52 +0530

    unused code

--- src/components/profileWizard.js
+++ src/components/profileWizard.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import { GoogleLogout } from 'react-google-login';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
